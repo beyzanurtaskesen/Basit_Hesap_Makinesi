2ebea3ea1dec1eba642e294acf67c9e116d991e9
Beyza Nur TAŞKESEN <Taskesennbeyzanurr@gmail.com>
2024-04-07 02:13:55 +0300

    Kullanıcıdan alınan iki değerin hangi işlemi yapmak istediği seçilerek istenilen işlemi  if-else yapılarıyla yapan  hesap makinesi prıgramı
    Java dersi örnek çalışmalarım

---
+++ Basit_Hesap_Makinesi/src/Hesap_Makinesi.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class Hesap_Makinesi {
+
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int ilkDeger,ikinciDeger;
+
+
+
+    }
+
+}
